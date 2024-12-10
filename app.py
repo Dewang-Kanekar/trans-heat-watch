b663e1d453fd9f40d36dc10e22847442aaddc22e
Dewang-Kanekar <dkkanekar2003@gmail.com>
2024-12-10 22:22:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import streamlit as st
 import numpy as np 
 
 st.header('Image Classification Model')
-model = load_model('Image_classify2.keras')
+model = load_model('D:\\Dewang\\python\\transformer_project1\\Image_classify2.keras')
 data_cat = ['cool_thermal_images', 
             'fire_img', 
             'hot_thermal images', 
             'short_ciruit_img']
 img_height = 240
 img_width = 240
-image =st.text_input('Enter Image name','a1.jpg')
+image =st.text_input('Enter Image name','D:\\Dewang\\python\\transformer_project1\\a1.jpg')
 
 image_load = tf.keras.utils.load_img(image, target_size=(img_height,img_width))
 img_arr = tf.keras.utils.array_to_img(image_load)
